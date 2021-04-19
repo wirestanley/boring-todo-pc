acc36edc31dda828aff116fecc1381e78a84b27c
andreamazzatxt <andreamazza89@gmail.com>
2021-04-19 09:59:26 +0200

    Ad TaskForm component and Button component

---
+++ src/components/TaskForm.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export default function TaskForm() {
+  return (
+    <div>
+      <textarea placeholder="Type your task"></textarea>
+      <Button />
+    </div>
+  );
+}
