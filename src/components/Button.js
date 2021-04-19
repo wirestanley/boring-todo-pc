544df5344e7ec4d5149ccf6426e8976e792c1470
andreamazzatxt <andreamazza89@gmail.com>
2021-04-19 10:32:07 +0200

    style button components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
+import style from "../styles/Button.module.css";
+
 export default function Button({ text, onClick }) {
   return (
     <div>
-      <button onClick={onClick}>{text}</button>
+      <button className={style.button} onClick={onClick}>
+        {text}
+      </button>
     </div>
   );
 }
