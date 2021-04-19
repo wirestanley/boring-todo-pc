443714d51e627705dd6ab6b0714466a6342b038a
andreamazzatxt <andreamazza89@gmail.com>
2021-04-19 11:08:50 +0200

    Handle line-crossing when task is done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,24 @@
 import style from "../styles/Task.module.css";
+import { useState } from "react";
 
 export default function Task({ task }) {
+  const [done, setDone] = useState(false);
+  const handleChange = () => {
+    setDone(!done);
+    console.log(done);
+  };
   return (
     <div className={style.container}>
-      <p className={style.text}>{task}</p>
+      <p
+        className={style.text}
+        style={{
+          textDecoration: done ? "line-through" : "none",
+        }}
+      >
+        {task}
+      </p>
       <div className={style.buttonWrapper}>
-        <input type="checkbox"></input>
+        <input type="checkbox" onChange={handleChange}></input>
         <p>X</p>
       </div>
     </div>
--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -1,10 +1,13 @@
 import Task from "./Task";
+import { useState } from "react";
 
 export default function TaskContainer({ tasks }) {
+  const [done, setDone] = useState(false);
+
   return (
     <div>
-      {tasks.map((task) => {
-        return <Task task={task} />;
+      {tasks.map((task, index) => {
+        return <Task task={task} key={`Task-${index}`} />;
       })}
     </div>
   );
