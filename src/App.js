cb72fbbc1beed9ae42b7a724362d29c45728a052
andreamazzatxt <andreamazza89@gmail.com>
2021-04-19 11:23:39 +0200

    Add counter in title

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Boring To Do List</h1>
+        <h1>Boring To Do List ({tasks.length})</h1>
         <TaskContainer tasks={tasks} removeFunction={removeTask} />
         <TaskForm addFunction={addTask} />
       </header>
