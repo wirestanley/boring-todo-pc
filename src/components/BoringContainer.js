5fc92a5ba5631a64170df99d1e75f9d91bb6a8eb
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-04-23 12:03:00 +0200

    final work

--- src/components/BoringContainer.js
+++ src/components/BoringContainer.js
@@ -3,8 +3,11 @@ import { getTask } from "../lib/fetch";
 import Button from "./Button";
 
 export default function BoringContainer({addTask, setBgColor}){
-const [boringTask, setBoringTask]= useState('')
+    const [counter, setCounter]= useState(0)
+    const [boringTask, setBoringTask]= useState('')
     const handleClick = () => {
+        setCounter(counter +1)
+        console.log(counter)
         setBoringTask ('thinking........') 
         setBgColor()
         getTask()
@@ -15,12 +18,16 @@ const [boringTask, setBoringTask]= useState('')
     const handleAdd = () => {
         addTask (boringTask)
         setBoringTask ('')
+        setCounter(0)
     }
 
 
     return (
         <div className= 'textstyles'>
-            <Button color = 'purple' onClick = {handleClick} text = "i am bored"/>
+            <Button 
+            color = 'purple' 
+            onClick = {handleClick} 
+            text =  {counter > 0 ? 'Still bored': 'I am bored'} />
            <p>{boringTask}<span className = 'heart' onClick = {handleAdd}>{boringTask &&'💚'}</span></p>
         </div>
     )
