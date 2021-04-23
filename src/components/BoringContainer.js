ab0f950bce919d00522473bda53a2764543413f1
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-04-23 10:28:23 +0200

    added an api function

--- src/components/BoringContainer.js
+++ src/components/BoringContainer.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { getTask } from "../lib/fetch";
 import Button from "./Button";
 
 export default function BoringContainer(){
+const [boringTask, setBoringTask]= useState('')
+    const handleClick = () => {
+        setBoringTask ('thinking........') 
+        getTask()
+        .then(response =>{
+            setBoringTask (response.activity)
+        })
+    }
+
     return (
         <div>
             <h1>boringContainer</h1>
-            <Button text = "i am bored"/>
+            <Button onClick = {handleClick} text = "i am bored"/>
+            <p>{boringTask}</p>
         </div>
     )
 }
